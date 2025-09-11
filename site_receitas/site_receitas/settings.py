7b1530b1bcb12116e9a72d9075a02fd43bed59d7
Lívia Lutz <108961867+livlutz@users.noreply.github.com>
2025-09-11 13:12:34 +0000

    add css + removing duplicate static

--- site_receitas/site_receitas/settings.py
+++ site_receitas/site_receitas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "receitas",
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -117,8 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
