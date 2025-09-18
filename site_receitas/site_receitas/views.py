1ada482545354e49189da6ef9c5422e84f38155a
lumpleme <108637505+lumpleme@users.noreply.github.com>
2025-09-18 11:43:34 +0000

    base para autenticação (TODO: migrar app usuarios)

--- site_receitas/site_receitas/views.py
+++ site_receitas/site_receitas/views.py
@@ -1,2 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 
+
+def homeSec(request):
+    '''
+    Renderiza a página inicial de segurança.
+    '''
+    return render(request, 'seguranca/homeSec.html')
+    
+def registro(request):
+    '''
+    Renderiza a página de registro de usuários.
+    '''
+    if request.method == 'POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('sec-home')
+    else:
+        formulario = UserCreationForm()
+    context = {'form': formulario, }
+    return render(request,
+        'seguranca/registro.html', context)
