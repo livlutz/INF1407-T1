a3b78a4cc459783e7fa135415466c561ad4971b0
lumpleme <108637505+lumpleme@users.noreply.github.com>
2025-09-28 20:30:27 +0000

    troca de senha sem quebrar :)

--- site_receitas/site_receitas/settings.py
+++ site_receitas/site_receitas/settings.py
@@ -133,3 +133,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'usuarios:login'
+LOGIN_REDIRECT_URL = 'receitas:homepage'
+LOGOUT_REDIRECT_URL = 'receitas:homepage'
